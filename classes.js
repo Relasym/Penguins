6e287d2d87acfbcbd7f017f434e8b3d0d8a75c99
Relasym <86927205+Relasym@users.noreply.github.com>
2021-07-26 11:36:02 +0200

    collision broken

---
+++ classes.js
@@ -0,0 +1,330 @@
+/*
+faction
+hasCollision = false;
+isDrawable = false;
+isUpdateable = false;
+isDestroying = false;
+affectedByGravity=false;
+
+*/
+class BasicObject {
+    faction;
+    hasCollision = false;
+    isDrawable = false;
+    isUpdateable = false;
+    isDestroying = false;
+    destructionProgress = 1.0; //destroys object if it reaches 0, used as a multiplier for color alpha
+
+    register() {
+        allObjects.push(this)
+        if (this.isDrawable) { drawableObjects.push(this) }
+        if (this.isUpdateable) { updateableObjects.push(this) }
+    }
+
+    deregister() {
+        allObjects.splice(allObjects.indexOf(this), 1)
+        if (this.isDrawable) { drawableObjects.splice(drawableObjects.indexOf(this), 1) }
+        if (this.isUpdateable) { updateableObjects.splice(updateableObjects.indexOf(this), 1) }
+    }
+
+    startDestruction() {
+        this.hasCollision = false
+        this.isDestroying = true;
+    }
+
+    update() {
+        if (this.isDestroying) {
+            this.destructionProgress -= 0.01;
+        }
+        if (this.destructionProgress <= 0) {
+            this.deregister();
+        }
+    }
+}
+class Circle extends BasicObject {
+    isDrawable = true;
+    hasCollision = true;
+    constructor(x, y, radius, color) {
+        super()
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = new jQuery.Color(color)
+    }
+    draw() {
+        context.beginPath()
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        if (this.isDestroying) {
+            let color = [...this.color._rgba];
+            color[3] *= this.destructionProgress;
+            context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        } else {
+            context.fillStyle = this.color;
+        }
+        context.fill()
+    }
+    update() {
+        super.update()
+    }
+}
+class MovingCircle extends Circle {
+    affectedByGravity = false;
+    isUpdateable = true;
+    velocity = { x: 0, y: 0 };
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color)
+    }
+    update() {
+        super.update();
+        if (this.affectedByGravity) {
+            this.velocity.y += gravity;
+        }
+        if (!this.isDestroying) {
+            this.x += this.velocity.x
+            this.y += this.velocity.y
+        }
+
+    }
+}
+class TestEnemy extends MovingCircle {
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color)
+    }
+    register() {
+        super.register();
+        objectsByFaction[this.faction].push(this);
+    }
+    startDestruction() {
+        super.startDestruction();
+        objectsByFaction[this.faction].splice(objectsByFaction[this.faction].indexOf(this), 1);
+    }
+}
+class Projectile extends MovingCircle {
+    hasCollision = true;
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color)
+    }
+    draw() {
+        super.draw()
+    }
+    register() {
+        super.register()
+        projectileObjects.push(this)
+        projectilesByFaction[this.faction].push(this);
+
+    }
+    deregister() {
+        super.deregister()
+        projectileObjects.splice(projectileObjects.indexOf(this), 1);
+        projectilesByFaction[this.faction].splice(projectilesByFaction[this.faction].indexOf(this), 1);
+    }
+}
+class Rectangle extends BasicObject {
+    hasCollision = true;
+    isDrawable = true;
+    rotation = 0 * Math.PI / 180;
+    constructor(x, y, width, height, color) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width
+        this.color = color;
+    }
+    draw() {
+        if (this.isDestroying) {
+            let color = [...this.color._rgba];
+            color[3] *= this.destructionProgress;
+            context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        } else {
+            context.fillStyle = this.color;
+        }
+
+        context.translate(this.x + this.width / 2, this.y + this.height / 2);
+        context.rotate(this.rotation);
+        context.translate(-1 * (this.x + this.width / 2), -1 * (this.y + this.height / 2));
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.setTransform(1, 0, 0, 1, 0, 0);
+    }
+    update() {
+        super.update();
+    }
+}
+class MovingRectangle extends Rectangle {
+    affectedByGravity = false;
+    isUpdateable = true
+    velocity = { x: 0, y: 0 };
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color)
+    }
+    update() {
+        super.update()
+        if (this.affectedByGravity) {
+            this.velocity.y += gravity;
+        }
+        if (!this.isDestroying) {
+            this.x += this.velocity.x
+            this.y += this.velocity.y
+        }
+
+    }
+
+}
+class Actor extends MovingRectangle {
+    refireDelay = 1000; //ms
+    lastFire = 0;
+    
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color)
+    }
+    register() {
+        super.register();
+        objectsByFaction[this.faction].push(this);
+    }
+    startDestruction() {
+        super.startDestruction();
+        objectsByFaction[this.faction].splice(objectsByFaction[this.faction].indexOf(this), 1);
+    }
+    update() {
+        super.update();
+        let currentTime = performance.now()
+        if (currentTime > this.lastFire + this.refireDelay) {
+            let projectile = new Projectile(this.x + this.width / 2, this.y + this.height / 2, 5, new jQuery.Color(this.color.rgba()))
+            projectile.velocity.x = (this.velocity.x > 0) ? this.velocity.x * 0.5 + 5 : this.velocity.x * 0.5 - 5;
+            projectile.velocity.y = this.velocity.y * 0.5;
+            projectile.faction = this.faction;
+            projectile.register();
+            this.lastFire = currentTime;
+        }
+
+    }
+}
+class Fish extends Actor {
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color)
+    }
+
+    draw() {
+        if (this.isDestroying) {
+            let color = [...this.color._rgba];
+            color[3] *= this.destructionProgress;
+            context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        } else {
+            context.fillStyle = this.color;
+        }
+
+        context.translate(this.x + this.width / 2, this.y + this.height / 2);
+        context.rotate(this.rotation);
+        context.translate(-1 * (this.x + this.width / 2), -1 * (this.y + this.height / 2));
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(fishImage, this.x, this.y);
+        context.setTransform(1, 0, 0, 1, 0, 0);
+    }
+
+}
+
+
+class Player extends MovingRectangle {
+    maxspeed = 5;
+    refireDelay = 50; //ms
+    lastFire;
+    projectileSpeed=10;
+    
+    constructor(x, y, width, height, color, speed) {
+        super(x, y, width, height, color)
+        this.speed = speed
+        this.lastFire = performance.now();
+    }
+    update() {
+        super.update()
+        // this.velocity = { x: 0, y: 0 }
+        //control
+        if (currentInputs.has("w")) { 
+            this.velocity.x+=this.speed*Math.sin(this.rotation)
+            this.velocity.y-=this.speed*Math.cos(this.rotation) }
+        if (currentInputs.has("s")) { 
+            this.velocity.x-=this.speed*Math.sin(this.rotation)
+            this.velocity.y+=this.speed*Math.cos(this.rotation) }
+        if (currentInputs.has("a")) { this.rotation-=0.03; }
+        if (currentInputs.has("d")) { this.rotation+=0.03; }
+        if (currentInputs.has(" ")) {
+            let currentTime = performance.now()
+            if (currentTime > this.lastFire + this.refireDelay) {
+                let projectile = new Projectile(this.x + this.width / 2, this.y + this.height / 2, 5, this.color)
+                projectile.velocity.x = 0.5*this.velocity.x + this.projectileSpeed*Math.sin(this.rotation);
+                projectile.velocity.y = 0.5*this.velocity.y + -1*this.projectileSpeed*Math.cos(this.rotation);
+                projectile.faction = this.faction;
+                projectile.register();
+                this.lastFire = currentTime;
+            }
+
+        }
+
+        //maximum "speed"
+        if(pyth(this.velocity.x,this.velocity.y)>this.maxspeed) {
+            this.velocity.x*=this.maxspeed/pyth(this.velocity.x,this.velocity.y);
+            this.velocity.y*=this.maxspeed/pyth(this.velocity.x,this.velocity.y);
+        }
+        // this.velocity.x = Math.min(this.velocity.x, this.maxspeed)
+        // this.velocity.x = Math.max(this.velocity.x, -1 * this.maxspeed)
+        // this.velocity.y = Math.min(this.velocity.y, this.maxspeed)
+        // this.velocity.y = Math.max(this.velocity.y, -1 * this.maxspeed)
+
+        //movement
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+
+        //"ground"
+        if (this.y > canvas.height - this.height) {
+            this.y = canvas.height - this.height
+            this.velocity.y = 0
+            this.velocity.x = 0
+        }
+    }
+    register() {
+        super.register();
+        objectsByFaction[this.faction].push(this);
+    }
+    startDestruction() {
+        super.startDestruction();
+        objectsByFaction[this.faction].splice(objectsByFaction[this.faction].indexOf(this), 1);
+    }
+    draw() {
+        if (this.isDestroying) {
+            let color = [...this.color._rgba];
+            color[3] *= this.destructionProgress;
+            context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
+        } else {
+            context.fillStyle = this.color;
+        }
+        
+        /*
+        steps:
+        move matrix to center of object
+        rotate
+        move matrix back to origin
+        draw
+        reset transformation matrix
+        */
+
+        context.translate(this.x + this.width / 2, this.y + this.height / 2);
+        context.rotate(this.rotation);
+        context.translate(-1 * (this.x + this.width / 2), -1 * (this.y + this.height / 2));
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(penguinImage, this.x, this.y);
+        context.setTransform(1, 0, 0, 1, 0, 0);
+    }
+
+
+}
+class TerrainRectangle extends Rectangle {
+    hasCollision = true;
+    constructor(x, y, width, height, color) {
+        super(x, y, width, height, color)
+    }
+    register() {
+        super.register();
+        objectsByFaction[this.faction].push(this);
+    }
+    startDestruction() { }
+}
---
+++ index.js
@@ -0,0 +1,330 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+const startTime = performance.now();
+const gravity = 0.1;
+const bounceRatio = 0.8;
+const currentInputs = new Set();
+const factionAmount = 10; //realistically no more than 3
+
+const penguinImage = document.getElementsByClassName("penguin").item(0);
+const fishImage = document.getElementsByClassName("fish").item(0);
+/*
+TODO
+disconnect animation speed from framerate
+add camera
+unify circle&rectangle objects
+
+later:
+level syntax
+add levels
+level generator
+
+
+*/
+
+allObjects = [];
+drawableObjects = [];
+updateableObjects = [];
+collisionObjects = [];
+terrainObjects = [];
+projectileObjects = [];
+
+const projectilesByFaction = []
+const objectsByFaction = []
+for (i = 0; i < factionAmount; i++) {
+    projectilesByFaction.push(new Array());
+    objectsByFaction.push(new Array());
+}
+
+let frameTime = startTime;
+let currentTime = startTime;
+
+
+function start() {
+    //html stat display, static part
+    $(".statlist .stat1 .type").html("allObjects: ");
+    $(".statlist .stat2 .type").html("drawableObjects: ");
+    $(".statlist .stat3 .type").html("updateableObjects: ");
+    $(".statlist .stat4 .type").html("collisionObjects: ");
+    $(".statlist .stat5 .type").html("terrainObjects: ");
+    $(".statlist .stat6").hide();
+    $(".statlist .stat7").hide();
+    $(".statlist .stat8").hide();
+    $(".statlist .stat9").hide();
+    $(".statlist .stat10 .type").html("frametime: ");
+
+    //create player
+    player = new Player(300, 300, 59, 100, new jQuery.Color("white"), 1);
+    player.hasCollision = true;
+    player.faction = 1;
+    player.affectedByGravity = false;
+    player.register();
+
+
+    //create circles
+    // for (i = 0; i < 10; i++) {
+    //     x = canvas.width * Math.random();
+    //     y = canvas.height * Math.random();
+    //     radius = 30 * Math.random() + 10;
+    //     speed = 0;
+    //     xvel = speed * (Math.random() - 0.5);
+    //     yvel = speed * (Math.random() - 0.5);
+    //     color = new jQuery.Color(`hsla(${Math.random() * 360},100%,50%,1)`);
+    //     testcircle = new TestEnemy(x, y, radius, color, true);
+    //     testcircle.velocity.x = xvel;
+    //     testcircle.velocity.y = yvel;
+    //     testcircle.faction = 2;
+    //     testcircle.register();
+    // }
+
+    //create rectangles
+    // for (i = 0; i < 10; i++) {
+    //     x = canvas.width * Math.random();
+    //     y = canvas.height * Math.random();
+    //     width = 45 * Math.random() + 10;
+    //     height = 45 * Math.random() + 10;
+    //     speed = 0;
+    //     xvel = speed * (Math.random() - 0.5);
+    //     yvel = speed * (Math.random() - 0.5);
+    //     color = new jQuery.Color(`hsla(${Math.random() * 360},100%,50%,1)`);
+    //     testRectangle = new Actor(x, y, width, height, color, true);
+    //     testRectangle.velocity.x = xvel;
+    //     testRectangle.velocity.y = yvel;
+    //     testRectangle.faction = 3;
+    //     testRectangle.register();
+    // }
+
+    for (i = 0; i < 10; i++) {
+        x = canvas.width * Math.random();
+        y = canvas.height * Math.random();
+        width = 100;
+        height = 50;
+        speed = 1;
+        xvel = speed * (Math.random() - 0.5);
+        yvel = speed * (Math.random() - 0.5);
+        color = new jQuery.Color("rgba(102,204,255,1)");
+        testRectangle = new Fish(x, y, width, height, color, true);
+        testRectangle.velocity.x = xvel;
+        testRectangle.velocity.y = yvel;
+        testRectangle.faction = 2;
+        testRectangle.register();
+    }
+
+    // create border walls
+    wallN = new TerrainRectangle(-100, -90, canvas.width + 200, 100, new jQuery.Color("blue"));
+    wallN.faction = 0;
+    wallN.register();
+    wallE = new TerrainRectangle(canvas.width - 10, -100, 100, canvas.height + 200, new jQuery.Color("blue"));
+    wallE.faction = 0;
+    wallE.register();
+    wallS = new TerrainRectangle(-100, canvas.height - 10, canvas.width + 200, 100, new jQuery.Color("blue"));
+    wallS.faction = 0;
+    wallS.register();
+    wallW = new TerrainRectangle(-90, -100, 100, canvas.height + 200, new jQuery.Color("blue"));
+    wallW.faction = 0;
+    wallW.register();
+    // wallCenter = new TerrainRectangle(400, -100, 100, canvas.height + 200, new jQuery.Color("blue"));
+    // wallCenter.faction = 0;
+    // wallCenter.register();
+
+
+    mainLoop();
+    console.log(objectsByFaction[0])
+
+}
+
+function mainLoop() {
+    //recursion
+    requestAnimationFrame(mainLoop);
+
+    //reset frame
+    context.fillStyle = "rgba(102,204,255,1)";
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //update objects
+    updateableObjects.forEach(object => {
+        object.update();
+    })
+
+    //collision testing after updating but before drawing
+    handleCollisions();
+
+    //cleanup offscreen objects
+    drawableObjects.forEach(object => {
+        if (object.x < -canvas.width || object.x > 2 * canvas.width || object.y < -canvas.height || object.y > 2 * canvas.height) {
+            object.deregister();
+        }
+    })
+
+    //draw objects
+    drawableObjects.forEach(object => {
+        object.draw();
+    })
+
+    //update stats
+    $(".statlist .stat1 .value").html(allObjects.length);
+    $(".statlist .stat2 .value").html(drawableObjects.length);
+    $(".statlist .stat3 .value").html(updateableObjects.length);
+    $(".statlist .stat4 .value").html(collisionObjects.length);
+    $(".statlist .stat5 .value").html(terrainObjects.length);
+    $(".statlist .stat10 .value").html(performance.now() - currentTime + "ms");
+    currentTime = performance.now();
+}
+
+/*collision functions */
+function handleCollisions() {
+    /*
+    TODO
+    first projectiles collide:
+        a, with non-faction projectiles
+        b, with non-faction actors
+        c, with terrain
+    second actors collide
+        a, with non-faction actors
+        b, with terrain
+    */
+
+
+    //Projectile collisions
+    for (let i = 0; i < projectilesByFaction.length; i++) { //faction 0 should not have projectiles?
+        //  projectile collides with other projectile
+        for (let j = 0; j < projectilesByFaction.length; j++) {
+            if (i != j) {
+                projectilesByFaction[i].forEach((projectile1) => {
+                    projectilesByFaction[j].forEach((projectile2) => {
+                        if (projectile1.hasCollision && projectile2.hasCollision && areObjectsColliding(projectile1, projectile2)) {
+                            console.log("proj proj coll")
+                            projectile1.startDestruction();
+                            projectile2.startDestruction();
+                        }
+                    })
+                })
+            }
+        }
+        //projectile collides with faction object other than faction 0 (terrain)
+        for (let j = 1; j < objectsByFaction.length; j++) {
+            if (i != j) {
+                projectilesByFaction[i].forEach((projectile) => {
+                    objectsByFaction[j].forEach((object) => {
+                        if (projectile.hasCollision && object.hasCollision && areObjectsColliding(projectile, object)) {
+                            console.log("proj act coll")
+                            projectile.startDestruction();
+                            object.startDestruction();
+                        }
+                    })
+                })
+            }
+        }
+        //projectile collides with faction 0 object (terrain)
+        if (i != 0) { //TODO let faction 0 projectiles collide with terrain?
+            projectilesByFaction[i].forEach((projectile) => {
+                objectsByFaction[0].forEach((object) => {
+                    if (projectile.hasCollision && object.hasCollision && areObjectsColliding(projectile, object)) {
+                        projectile.startDestruction();
+                        console.log("proj terr coll");
+                    }
+                })
+            })
+        }
+    }
+
+    for (let i = 1; i < objectsByFaction.length; i++) {
+        for (let j = i + 1; j < objectsByFaction.length; j++) {
+            if (i != j) {
+                for (object1 of objectsByFaction[i]) {
+                    for (object2 of objectsByFaction[j]) {
+                        if (object1.hasCollision && object2.hasCollision && areObjectsColliding(object1, object2)) {
+                            // TODO Collisions between objects from 
+                        }
+                    }
+                }
+            }
+        }
+
+        for (object1 of objectsByFaction[i]) {
+            for (object2 of objectsByFaction[0]) {
+                if (object1.hasCollision && object2.hasCollision && areObjectsColliding(object1, object2)) {
+                    object1.velocity = { x: 0, y: 0 };
+                }
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+function areObjectsColliding(object1, object2) {
+    name1 = object1.constructor.name;
+    name2 = object2.constructor.name;
+    if (name1 == "Rectangle" || name1 == "MovingRectangle" || name1 == "Player" || name1 == "TerrainRectangle" || name1 == "Actor" || name1 == "Fish") {
+        if (name2 == "Rectangle" || name2 == "MovingRectangle" || name2 == "Player" || name2 == "TerrainRectangle" || name2 == "Actor" || name1 == "Fish") {
+            return collisionRectangleRectangle(object1, object2);
+        } else {
+            return collisionRectangleCircle(object1, object2);
+        }
+    } else {
+        if (name2 == "Rectangle" || name2 == "MovingRectangle" || typeof name2 == "Player" || name2 == "TerrainRectangle" || name2 == "Actor" || name1 == "Fish") {
+            return collisionRectangleCircle(object2, object1); //<- IMPORTANT
+        } else {
+            return collisionCircleCircle(object1, object2);
+        }
+    }
+}
+
+function collisionRectangleRectangle(rectangle1, rectangle2) {
+    return (rectangle1.x < rectangle2.x + rectangle2.width &&
+        rectangle1.x + rectangle1.width > rectangle2.x &&
+        rectangle1.y < rectangle2.y + rectangle2.height &&
+        rectangle1.y + rectangle1.height > rectangle2.y)
+}
+function collisionRectangleCircle(rectangle, circle) {
+    xborder = circle.x
+    yborder = circle.y
+    if (circle.x < rectangle.x) xborder = rectangle.x
+    else if (circle.x > (rectangle.x + rectangle.width)) xborder = rectangle.x + rectangle.width
+    if (circle.y < rectangle.y) yborder = rectangle.y
+    else if (circle.y > (rectangle.y + rectangle.height)) yborder = rectangle.y + rectangle.height
+    dist = Math.sqrt((circle.x - xborder) ** 2 + (circle.y - yborder) ** 2)
+    return (dist <= circle.radius)
+}
+function collisionCircleCircle(circle1, circle2) {
+    return (pyth(circle1.x - circle2.x, circle1.y - circle2.y) <= (circle1.radius + circle2.radius))
+}
+function pyth(x, y) {
+    return Math.sqrt(x * x + y * y)
+}
+function normalize(vector) {
+    length = pyth(vector.x, vector.y)
+    return { x: vector.x / length, y: vector.y / length }
+}
+
+
+/* logging pressed keys */
+document.addEventListener('keydown', keypress => {
+    currentInputs.add(keypress.key);
+    // console.log(pressedKeys)
+});
+document.addEventListener('keyup', keypress => {
+    currentInputs.delete(keypress.key);
+});
+document.addEventListener('mousedown', btn => {
+    currentInputs.add("MB" + btn.button)
+    console.log(currentInputs)
+    // console.log(btn)
+    // console.log(canvas)
+    mouseX = btn.clientX - canvas.offsetLeft;
+    mouseY = btn.clientY - canvas.offsetTop;
+    // console.log(mouseX + " " + mouseY)
+});
+document.addEventListener('mouseup', btn => {
+    currentInputs.delete("MB" + btn.button)
+});
+
+start()
+
